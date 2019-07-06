665db4ffd882f958a149c6498b36dc9c3205c8a3
Mark Havens <mark.r.havens@gmail.com>
2019-07-05 22:33:09 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,4 +15,5 @@ if [ ! -f "/app/workers/reports/config/production.toml" ] ; then
         cp /app/config/docker-production.toml.tmpl /app/workers/reports/config/production.toml
     fi
 fi
+export PORT="3000"
 exec "$@"
